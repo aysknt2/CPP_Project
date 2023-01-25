a5196156de09a93f40d66b3c26aba3f5aa1c61e2
aysknt2 <105555103+aysknt2@users.noreply.github.com>
2023-01-25 19:22:14 +0530

    Create Day_3_Student_resullts_sys.cpp

---
+++ Day_3_Student_resullts_sys.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int roll, age, m_mark, p_mark, c_mark;
+string name;
+
+void addMember(void){
+  system("cls");
+
+  cout<<"Name : ";
+  cin>>name;
+  cout<<"Age : ";
+  cin>>age;
+  cout<<"Roll Number : ";
+  cin>>roll;
+  cout<<"Marks in Mathematics : ";
+  cin>>m_mark;
+  cout<<"Marks in Physics : ";
+  cin>>p_mark;
+  cout<<"Marks in Chemistry : ";
+  cout<<endl<<endl<<endl<<"Student is added. Thank you."<<"Press any key to continue...";
+  getch();
+}
+
+int main(){
+
+return 0;
+}
